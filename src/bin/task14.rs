edeab918f7d7e69ae1d4e63eae9f3d0c6bf348c0
Julian Rode <julian.rode@tu-dresden.de>
2023-12-16 23:06:23 +0100

    added task 16 2part

--- src/bin/task14.rs
+++ src/bin/task14.rs
@@ -2,7 +2,7 @@ use std::{
     cmp::{max, min},
     fs::File,
     io::{BufRead, BufReader}, num,
-    collections::HashMap,
+    collections::HashMap, usize,
 };
 
 const TEXT: &str = "O....#....
@@ -29,6 +29,45 @@ fn get_lines(is_test: bool) -> Box<dyn Iterator<Item = String>> {
     }
 }
 const FILE_PATH: &str = "dat/task14.txt";
+
+fn move_sn( map :& mut Vec<Vec<char>>, size_x : usize, size_y : usize, n : bool ) {
+    for x in 0..size_x{ 
+        let mut skip : i32=0;
+        for yy in 0..size_y{
+            let y =  if n {yy} else {size_y - 1 - yy};
+            match map[y][x] {
+                '#' =>  skip = 0 , 
+                '.' => skip += if n {1} else {-1} ,
+                'O' => {
+                    if skip!=0{
+                    map[(y as i32 - skip) as usize][x] = 'O'; 
+                    map[y][x] = '.';
+                    }
+                    }, 
+                _ => unreachable!("s"),
+            }        
+        }
+    }
+}
+fn move_ew( map :& mut Vec<Vec<char>>, size_x : usize, size_y : usize, w : bool ) {
+    for x in 0..size_y{ 
+        let mut skip : i32=0;
+        for yy in 0..size_x{
+            let y =  if w {yy} else {size_y - 1 - yy};
+            match map[x][y] {
+                '#' =>  skip = 0 , 
+                '.' => skip += if w {1} else {-1} ,
+                'O' => {
+                    if skip!=0{
+                    map[x][(y as i32 - skip) as usize] = 'O'; 
+                    map[x][y] = '.';
+                    }
+                    }, 
+                _ => unreachable!("s"),
+            }        
+        }
+    }
+}
 
 
 fn main() {
@@ -57,7 +96,45 @@ fn main() {
         }
     } 
     println!("res {}",total);
-
-
+    //map.iter().for_each(|f| println!("{}", f.iter().collect::<String>()));
+    println!("  s");
+    let mut hist : Vec<Vec<Vec<char>>> = Vec::new(); 
+    let mut run = true;
+    let mut nloop = 0;
+    let mut ninit = 0;
+    for i in 0..999999{
+        hist.push( map.clone() );
+        move_sn(&mut map,size_y,size_x,true);
+        move_ew(&mut map,size_y,size_x,true);
+        move_sn(&mut map,size_y,size_x,false);
+        move_ew(&mut map,size_y,size_x,false);
+        for (j,map2) in hist.iter().enumerate(){
+            if *map2==map {
+                println!("i {} j {}",i,j); 
+                nloop = i - j + 1;
+                ninit = j;
+                run=false; 
+                break;}
+        }
+        if !run{
+            break;
+        }
+    }
+    let remaining = (1000000000 - ninit) % nloop; 
+    println!("ninit {} nloop {}  r {}",ninit,nloop,remaining); 
+    for _i in 0..remaining{
+        move_sn(&mut map,size_y,size_x,true);
+        move_ew(&mut map,size_y,size_x,true);
+        move_sn(&mut map,size_y,size_x,false);
+        move_ew(&mut map,size_y,size_x,false);
+    }
+    total = 0;
+    for x in 0..size_x{
+        for y in 0..size_y{
+            total += if map[y][x]=='O' {size_y-y} else {0};
+        }
+    }
+    println!("total {}",total); 
+    //map.iter().for_each(|f| println!("{}", f.iter().collect::<String>()));
 
 }
